7f656040b9241044dbcff6dc4dfd4e2b810ca8b8
Monalisa Singh <singh.monalisa3@gmail.com>
2024-08-17 11:56:16 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginPage from "./LoginForm"; // Adjust the path if necessary
+
+function App() {
+  return (
+    <div>
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/LoginForm.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import './Login.css';
+
+function LoginPage() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (username === 'user' && password === 'password') {
+            setIsLoggedIn(true);
+            setMessage('Welcome, user!');
+        } else {
+            setMessage('Invalid username or password');
+        }
+    };
+
+    if (isLoggedIn) {
+        return (
+            <div>
+                <h1>Login Page</h1>
+                <p>{message}</p>
+            </div>
+        );
+    }
+
+    return (
+        <div>
+            <h1>Login Page</h1>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Username:</label>
+                    <input
+                        type="text"
+                        placeholder="username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label>Password:</label>
+                    <input
+                        type="password"
+                        placeholder="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+            {message && <p className="warning">{message}</p>}
+        </div>
+    );
+}
+
+export default LoginPage;
